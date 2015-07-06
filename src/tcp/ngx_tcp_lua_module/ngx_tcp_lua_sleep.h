57b7cae8db773dd6eafdb328e00d374cc0963c8c
laozi2 <sunboy20475@163.com>
2015-07-06 18:53:38 +0800

    log 2015.7.6-3

--- src/tcp/ngx_tcp_lua_module/ngx_tcp_lua_sleep.h
+++
@@ -1,1 +0,0 @@
-/* * Copyright (C) Xiaozhe Wang (chaoslawful) * Copyright (C) Yichun Zhang (agentzh) */#ifndef _NGX_TCP_LUA_SLEEP_H_INCLUDED_#define _NGX_TCP_LUA_SLEEP_H_INCLUDED_#include "ngx_tcp_lua_common.h"void ngx_tcp_lua_inject_sleep_api(lua_State *L);void ngx_tcp_lua_sleep_cleanup(void *data);#endif /* _NGX_TCP_LUA_SLEEP_H_INCLUDED_ */
